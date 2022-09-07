bf069c95d2c631e9d7981d4277a87c933bd8b18e
Usborn Ocampo <usborn116@gmail.com>
2022-09-07 12:41:03 -0700

    configured mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,8 @@ Rails.application.configure do
   config.cache_classes = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.perform_deliveries = true
+
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
